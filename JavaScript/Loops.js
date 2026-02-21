2492b9baabd9675c22f1c93e555b02b47bd3e20d
mridul sharma <smridul925@gmail.com>
2026-02-21 23:42:57 +0530

    JavaScript

--- JavaScript/Loops.js
+++ JavaScript/Loops.js
@@ -62,9 +62,9 @@
 //     console.log(char);
 // }
 // Nuested array
-// let heroes = [["superman" , "batman" , "wonder woman"] , ["spiderman" , "ironman" , "thor"]];
-// for(list of heroes){
-//     for(hero of list){
-//         console.log(hero);
-//     }
-// }
+let heroes = [["superman" , "batman" , "wonder woman"] , ["spiderman" , "ironman" , "thor"]];
+for(list of heroes){
+    for(hero of list){
+        console.log(hero);
+    }
+}
