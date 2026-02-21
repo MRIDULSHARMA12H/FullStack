935151937e4dac832bbc7123657de74a9e8d2fb6
mridul sharma <smridul925@gmail.com>
2026-02-21 23:39:03 +0530

    Updated topics.

--- JavaScript/Loops.js
+++ JavaScript/Loops.js
@@ -26,14 +26,45 @@
 //     guess = prompt("Please try again");
 // }
 // if(guess == fevmovie){
-    console.log("congrats!!");
+    // console.log("congrats!!");
 // }
 
 
 
 // Loops with Arrays
 
-let fruits = ["mango" , "apple" , "banana" , "orange"];
-for(let i = 0 ; i<fruits.length ; i++){
-    console.log(i,fruits[i]);
-}
+// let fruits = ["mango" , "apple" , "banana" , "orange"];
+// for(let i = 0 ; i<fruits.length ; i++){
+//     console.log(i,fruits[i]);
+// }
+
+
+// Nuested loop with Arrays
+
+// let Student = [["aman" , 95] , ["Mridul" , 94.4] , ["Karan" , 100]];
+// for(let i = 0 ; i<Student.length ; i++){
+//     for(let j = 0 ; j<Student[i].length ; j++){
+//         console.log(Student[i][j]);
+//     }
+// }
+
+
+// for of loop 
+
+// let fruits = ["mango" , "banana" , "apple" , "litchi"];
+// for(fruit of fruits){
+//     console.log(fruit);
+// }
+
+// let String  = ["apnacollege"];
+
+// for(char of String){
+//     console.log(char);
+// }
+// Nuested array
+// let heroes = [["superman" , "batman" , "wonder woman"] , ["spiderman" , "ironman" , "thor"]];
+// for(list of heroes){
+//     for(hero of list){
+//         console.log(hero);
+//     }
+// }
