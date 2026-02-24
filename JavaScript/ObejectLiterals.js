c5238dd1ff6a8573d0473199600d59a8251dd9da
mridul sharma <smridul925@gmail.com>
2026-02-24 23:16:21 +0530

    Objects

---
+++ JavaScript/ObejectLiterals.js
@@ -0,0 +1,32 @@
+let student ={
+    name : "Mridul",
+    age : 20,
+    marks : [80 , 90 , 70]
+};
+
+let mark = "marks";
+// console.log(student[mark]);
+// console.log(student.marks);
+
+// Add / update value
+let students = {
+    name : "Mridul Sharma",
+    age : 22,
+    marks : 90.2,
+    city : "Delhi"
+};
+
+// console.log(students.city);
+// students.city = "Mumbai";
+// console.log(students.city);
+
+// Add
+
+// students.gender = "male";
+// console.log(students);
+// console.log(students.gender);
+
+//  Update
+
+// students.marks = "A";
+// console.log(students.marks);
