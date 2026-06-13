ef31d2e7dd6ddbdb0ad35296f7c3f99ef52926f4
mridul sharma <smridul925@gmail.com>
2026-06-13 17:46:36 +0530

    DOM concept

---
+++ DOMConcept/app.js
@@ -0,0 +1,10 @@
+// let smallImg = document.getElementsByClassName("oldImg");
+// for(let i =0 ; i<smallImg.length;i++){
+//     console.dir(smallImg[i].src);
+// }
+// console.dir(smallImg[0].src = "photos/creation_3.jpeg");
+
+// let pra = document.getElementsByTagName("p")[1];
+// console.dir(pra);
+
+// console.dir(document.getElementsByTagName("abc"));
